cafb79585937d16ce7b5faef0adba446b35edf00
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2020-11-26 10:10:36 +0200

    dimaarion/react-catalog

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const [product, setProduct] = useState([{}]);
+  useEffect(() => {
+    axios.get("/db/product.json").then((response) => setProduct(response));
+  }, []);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
