142161e6b881b5941895a5ad1c12ab2ca973a53f
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2020-11-26 11:02:17 +0200

    dimaarion/react-catelog

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Container, Nav, List } from "bootstrap-4-react";
 import "./styles.css";
+import Product from "./Product";
 
 export default function App() {
   const [product, setProduct] = useState([{}]);
   useEffect(() => {
-    axios.get("/db/product.json").then((response) => setProduct(response));
+    axios.get("/db/product.json").then((response) => setProduct(response.data));
   }, []);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container className="App text-left">
+      <h1>Каталог товаров</h1>
+      <Nav flex="column">
+        {product.map((x) => (
+          <List.Item key={x.id + 10}>
+            <Product />
+          </List.Item>
+        ))}
+      </Nav>
+    </Container>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Row } from "bootstrap-4-react";
+export default function Product(props) {
+  return <Row>Active</Row>;
+}
